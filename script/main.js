fe1e0c0e988a917e3bed01da4552dd3d49060e8d
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-01 13:37:26 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,6 @@
+/* 
+* A script printing all integer n, being (1 <= n <= 100), provided that: multiples of both 3 and 5 are substitute with the string "FizzBuzz"; multiples of 3 but not 5 with "Fizz"; and multiples of 5 but not 3 with "Buzz".
+*/
+
+console.log('main. js is working')
+
