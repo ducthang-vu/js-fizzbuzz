5d847d3d230affa2af05bd7cd052a3202bf4e6ca
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-01 14:18:16 +0200

    refactoring

--- script/main.js
+++ script/main.js
@@ -5,13 +5,13 @@
 console.log('main. js is working')
 
 for (i = 1; i < 101; i++) {
-    if ((i % 3 == 0) && (i % 5 == 0)) {
+    if (i % 3 == 0 && i % 5 == 0) {
         console.log('FizzBuzz')
     }
-    else if (i % 3 == 0 && !(i % 5 == 0)) {
+    else if (i % 3 == 0 && i % 5 != 0) {
         console.log('Fizz')
     }
-    else if (i % 5 == 0 && !(i % 3 == 0)) {
+    else if (i % 5 == 0 && i % 3 != 0) {
         console.log('Buzz')
     }
     else {
