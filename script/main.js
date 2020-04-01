a3ddd623c2ded5e7a5a4989021b7670a6ec01b1f
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-01 13:42:28 +0200

    completed

--- script/main.js
+++ script/main.js
@@ -1,6 +1,20 @@
 /* 
-* A script printing all integer n, being (1 <= n <= 100), provided that: multiples of both 3 and 5 are substitute with the string "FizzBuzz"; multiples of 3 but not 5 with "Fizz"; and multiples of 5 but not 3 with "Buzz".
+* A script printing all integers n, being (1 <= n <= 100), provided that: multiples of both 3 and 5 are substituted with the string "FizzBuzz"; multiples of 3 but not 5 with "Fizz"; and multiples of 5 but not 3 with "Buzz".
 */
 
 console.log('main. js is working')
 
+for (i = 1; i < 100; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log('FizzBuzz')
+    }
+    else if (i % 3 == 0 && !(i % 5 == 0)) {
+        console.log('Fizz')
+    }
+    else if (i % 5 == 0 && !(i % 3 == 0)) {
+        console.log('Buzz')
+    }
+    else {
+        console.log(i)
+    }
+}
